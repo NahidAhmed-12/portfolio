fa119fe9eb8d93e892f7f7cff4c6b973d1b1b02c
𝑵𝒂𝒉𝒊𝒅 𝑨𝒉𝒎𝒆𝒅 <marupahmed92@gmail.com>
2025-11-12 15:35:18 +0600

    Revise development philosophy description
    Updated the description of development philosophy.

--- src/pages/Vision.jsx
+++ src/pages/Vision.jsx
@@ -7,7 +7,7 @@ function Vision() {
             <div  className='bg-[#3D3D543D] px-3 z-50 rounded-2xl flex flex-col items-center justify-center py-5'>
                 <h2 className='text-xl font-semibold' >My Development Philosophy</h2>
                 <p className='text-primary max-w-xl py-3 text-2xl text-center'>
-                My goal is to build seamless and engaging digital experiences by writing clean, efficient, and scalable code. I believe in the power of technology to connect people and create meaningful interactions.
+                Crafting intuitive and engaging digital experiences with clean, user-focused code.
                 </p>
             </div>
         </section>

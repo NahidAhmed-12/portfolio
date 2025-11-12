6dcce9a0ac433694fa0b695e1adce8bdb60acd27
𝑵𝒂𝒉𝒊𝒅 𝑨𝒉𝒎𝒆𝒅 <marupahmed92@gmail.com>
2025-11-12 15:33:27 +0600

    Update Vision.jsx

--- src/pages/Vision.jsx
+++ src/pages/Vision.jsx
@@ -5,9 +5,10 @@ function Vision() {
     <>
         <section className='md:py-10 lg:px-24 px-5'>
             <div  className='bg-[#3D3D543D] px-3 z-50 rounded-2xl flex flex-col items-center justify-center py-5'>
-                <h2 className='text-xl font-semibold' >Wern&apos;s Vision</h2>
-                <p className='text-primary max-w-xl py-3 text-2xl text-center md:text-start'>Building a financial future where everyone has 
-                access to secure and effortless digital money.</p>
+                <h2 className='text-xl font-semibold' >My Development Philosophy</h2>
+                <p className='text-primary max-w-xl py-3 text-2xl text-center'>
+                My goal is to build seamless and engaging digital experiences by writing clean, efficient, and scalable code. I believe in the power of technology to connect people and create meaningful interactions.
+                </p>
             </div>
         </section>
     </>
